f6c1e7f295b6dcf86a9318cc5f671a6a8ae4b719
zachglassman <zach.glassman@gmail.com>
2018-10-15 18:45:20 -0700

    proper app name

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,5 +1,5 @@
 import os
-from app import app as create_app
+from app import create_app
 
-app = create_app(os.environ['SECRET_KEY'], 
+application = create_app(os.environ['SECRET_KEY'], 
                  os.environ['DATABASE_URI'])
